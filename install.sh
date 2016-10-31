99f2b3b5d2c17360937f0b12cc6ff75048646838
lucas-burdell <lucas.burdell@blackburn.edu>
2016-10-31 20:03:05 +0000

    add touch

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm -f ~/.nanorc
 
 git clone https://github.com/scopatz/nanorc.git /tmp/nanorc
 mv -f /tmp/nanorc/*.nanorc /usr/share/nano/
-
+touch ~/.nanorc
 for f in /usr/share/nano/*.nanorc
 do
 echo "include $f" >> ~/.nanorc
